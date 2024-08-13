2fc208fb0d67aa091876f4149f771d8ff0d69cf8
Felipe <huertasguarinfelipez2@gmail.com>
2024-08-13 12:15:50 -0500

    Feature/Se crea la funcion para hallar el valor esperado de f(xi + h)

--- SeriesDeTaylor.py
+++ SeriesDeTaylor.py
@@ -4,7 +4,7 @@ import sympy as sp
 # Definir la variable simbólica
 x = sp.symbols('x')
 
-#Prueba de Arrays
+#Definir Variables
 funciones = [
     -0.1 * x**4,  
     -0.15 * x**3,
@@ -13,11 +13,24 @@ funciones = [
     1.2   
 ]
 
-primeraDerivada = []
+xi = 0
+h = 1
 
+#Calcular el valor de f(xi + h)
+def valorEsperado(xi, h, funciones):
+    sumatoria = 0
+    x = xi + h
+    for i in range(len(funciones)):
+        prueba = str(funciones[i])
+        resultado = eval(prueba)
+        sumatoria += resultado
+    print(f"el total de f(x) es :{sumatoria}")
+
+#Derivadas
+primeraDerivada = []
 for i in range(len(funciones)):
-    print(i)
     derivada = sp.diff(funciones[i],x)
-    print(f"primera derivada: {derivada}")
+    #print(f"primera derivada: {derivada}")
     primeraDerivada.append(derivada)
 
+valorEsperado(xi, h, funciones)
